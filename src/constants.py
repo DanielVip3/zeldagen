946b0027c5c983328ba363298b23fb559c36feae
Daniele De Martino <danieledemartino.72004@gmail.com>
2024-12-29 18:31:05 +0100

    ✨ Added mutation logic Four different mutation strategies, chosen randomly but preventing illegal states.

--- src/constants.py
+++ src/constants.py
@@ -6,6 +6,9 @@ NUM_ROOMS = 10
 # Index of root element in graph
 ROOT = 0
 
+# Index of finish element in graph
+FINISH = 9
+
 # 0: beginning room (S)
 # 1: key room (K)
 # 2: normal room (N)
--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from constants import Types
+from constants import Types, ROOT, FINISH, NUM_ROOMS
 from dungeon import Dungeon
 from copy import deepcopy
 import random
@@ -39,6 +39,56 @@ def crossover(parent1, parent2):
 
     return child1, child2
 
+def should_lock(individual):
+    locked_edges = [(node1, node2) for node1, node2, attr in individual.graph.edges(data=True) if attr.get('locked')]
+    return len(locked_edges) < (NUM_ROOMS // 3)
+
+def mutation(individual):
+    while True:
+        action = random.randint(1, 4)
+
+        if action == 1: # add an edge
+            if len(individual.graph.edges) >= (NUM_ROOMS + (NUM_ROOMS // 3)):
+                continue
+
+            node = random.choice(range(ROOT + 1, FINISH))
+
+            connected = set(nx.all_neighbors(individual.graph, node))
+            connected.add(node)
+
+            possible_connections = [node for node, attr in individual.graph.nodes(data=True) if node not in connected and attr.get('type') not in [Types.START, Types.FINISH]]
+            if not possible_connections:
+                continue
+
+            node_to_connect = random.choice(possible_connections)
+
+            individual.graph.add_edge(node, node_to_connect, locked=should_lock(individual))
+        elif action == 2: # remove an edge
+            if len(individual.graph.edges) < (NUM_ROOMS + (NUM_ROOMS // 3)):
+                continue
+
+            possible_edges = [(node1, node2) for node1, node2, attr in individual.graph.edges(data=True)
+                              if individual.graph.nodes[node2]['type'] != Types.FINISH and individual.graph.in_degree(node2) > 1]
+            if not possible_edges:
+                continue
+
+            edge = random.choice(possible_edges)
+            individual.graph.remove_edge(*edge)
+        elif action == 3: # change node type
+            node = random.choice(range(ROOT + 1, FINISH))
+            individual.graph.nodes[node]['type'] = random.choice([Types.NORMAL, Types.KEY])
+        elif action == 4: # toggle edge lock
+            to_lock = should_lock(individual)
+
+            possible_edges = [(node1, node2) for node1, node2, attr in individual.graph.edges(data=True)
+                              if individual.graph.nodes[node2]['type'] != Types.FINISH and attr.get('locked') != to_lock]
+            if not possible_edges:
+                continue
+
+            edge = random.choice(possible_edges)
+            individual.graph.edges[edge]['locked'] = to_lock
+        break
+
 def genetic_algorithm(population_size = 10):
     # Initialize population
     population = [Dungeon() for i in range(population_size)]
@@ -46,10 +96,7 @@ def genetic_algorithm(population_size = 10):
 genetic_algorithm()
 
 d1 = Dungeon()
-d2 = Dungeon()
 d1.print()
-d2.print()
-c1, c2 = crossover(d1, d2)
-c1.print()
-c2.print()
+mutation(d1)
+d1.print()
 # print(d.shortest_path_least_keys(0))
